b1fcb7a4eaa81e59e1aa30b21c2c98ab17d54ddb
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-08 22:45:29 +0200

    Add task Merge Sorted Arrays

---
+++ CoderPad.MergeSortedArrays.py
@@ -0,0 +1,35 @@
+# Задача 2 — Merge Sorted Arrays
+# Условие:
+# Даны два отсортированных массива целых чисел arr1 и arr2.
+# Нужно вернуть новый отсортированный массив, содержащий все элементы обоих.
+#
+# Пример:
+# Input: [1,3,5], [2,4,6]
+# Output: [1,2,3,4,5,6]
+
+# Условия:
+# Нельзя использовать готовые методы типа Arrays.sort() для всего результата
+# Нужно сделать слияние за O(n+m)
+
+def mergeSortedArrays(arr1, arr2):
+    merged_array = []
+    i, j = 0, 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:
+            merged_array.append(arr1[i])
+            i += 1
+        else:
+            merged_array.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        merged_array.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        merged_array.append(arr2[j])
+        j += 1
+
+    return merged_array
+
+
+print(mergeSortedArrays([1,3,5],[2,4,6]))
