42bdf2e9ce290f0bf1d72121618b8f5b2abacfb5
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-08 23:10:58 +0200

    optimize task Valid Parentheses

--- CoderPad.ValidParentheses.py
+++ CoderPad.ValidParentheses.py
@@ -15,29 +15,38 @@
 # Output: false
 
 
-def validParentheses(s: str):
-    result = True
-    for i, el in enumerate(s):
-        if el == '(':
-            if s[i+1] == ')':
-                continue
-            else:
-                result = False
-                break
-        if el == '{':
-            if s[i+1] == '}':
-                continue
-            else:
-                result = False
-                break
-        if el == '[':
-            if s[i+1] == ']':
-                continue
-            else:
-                result = False
-                break
+def validParentheses(s: str) -> bool:
+    # result = True
+    # for i, el in enumerate(s):
+    #     if el == '(':
+    #         if s[i+1] == ')':
+    #             continue
+    #         else:
+    #             result = False
+    #             break
+    #     if el == '{':
+    #         if s[i+1] == '}':
+    #             continue
+    #         else:
+    #             result = False
+    #             break
+    #     if el == '[':
+    #         if s[i+1] == ']':
+    #             continue
+    #         else:
+    #             result = False
+    #             break
+    stack = []
+    mapping = {')': '(', '}': '{', ']': '['}
+    for ch in s:
+        if ch in mapping.values():
+            stack.append(ch)
+        elif ch in mapping:
+            if not stack or stack[-1] != mapping[ch]:
+                return False
+            stack.pop()
 
-    return result
+    return not stack
 
 
 print(validParentheses("()[]{}"))
