6364215490cb77dbeaabb1b8160771d59d2cb923
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-09 22:40:55 +0200

    update task CheckAnagram

--- CoderPad.CheckAnagram.py
+++ CoderPad.CheckAnagram.py
@@ -1,13 +1,26 @@
 # Задача 1 — Проверка анаграммы
-# Даны две строки s и t. Определи, являются ли они анаграммами (содержат одинаковые буквы в любом порядке).
+# Даны две строки s и t. Определи, являются ли они анаграммами,
+# то есть содержат ли они одинаковые буквы в одинаковом количестве, но, возможно, в другом порядке.
+# Считаем, что входные строки содержат только строчные латинские буквы.
+#
+# Пример:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+#
+# Input: s = "rat", t = "car"
+# Output: false
 
 
 def isAnagram(s: str, t: str) -> bool:
+    t_list = []
+    for i in t:
+        t_list.append(i)
     for i in s:
-        for j in t:
-            if i == j:
-                return True
+        if i in t_list:
+            t_list.remove(i)
+    if not t_list:
+        return True
     return False
 
 
-print(isAnagram("LEET","CODE"))
+print(isAnagram("rat", "car"))
