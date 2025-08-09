c0622b7fd5f24d9a84383c8824feb24f0a67c819
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-08-09 23:15:13 +0200

    optimize task Check Anagram

--- CoderPad.CheckAnagram.py
+++ CoderPad.CheckAnagram.py
@@ -10,17 +10,22 @@
 # Input: s = "rat", t = "car"
 # Output: false
 
+from collections import Counter
+
 
 def isAnagram(s: str, t: str) -> bool:
-    t_list = []
-    for i in t:
-        t_list.append(i)
-    for i in s:
-        if i in t_list:
-            t_list.remove(i)
-    if not t_list:
-        return True
-    return False
+    # t_list = []
+    # for i in t:
+    #     t_list.append(i)
+    # for i in s:
+    #     if i in t_list:
+    #         t_list.remove(i)
+    # if not t_list:
+    #     return True
+    # return False
+    if len(s) != len(t):
+        return False
+    return Counter(s) == Counter(t)
 
 
 print(isAnagram("rat", "car"))
